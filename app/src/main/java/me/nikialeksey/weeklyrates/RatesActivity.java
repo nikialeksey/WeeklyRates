964ab961e69486b9498cf21746560ef237e698b4
Alexey Nikitin <nikialeksey@gmail.com>
2017-03-05 23:11:03 +0700

    Initial commit

---
+++ app/src/main/java/me/nikialeksey/weeklyrates/RatesActivity.java
@@ -0,0 +1,13 @@
+package me.nikialeksey.weeklyrates;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rates_activity);
+    }
+}
