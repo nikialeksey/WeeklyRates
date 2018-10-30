9b5860685ecad745f1b528c2b29783e350a10a42
Alexey Nikitin <nikialeksey@gmail.com>
2018-10-31 00:55:06 +0700

    Add chart Add stetho realm browser

---
+++ app/src/debug/java/me/nikialeksey/weeklyrates/DebugApp.java
@@ -0,0 +1,19 @@
+package me.nikialeksey.weeklyrates;
+
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+public class DebugApp extends WeeklyRatesApp {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(
+                                RealmInspectorModulesProvider.builder(this).build()
+                        )
+                        .build()
+        );
+    }
+}
